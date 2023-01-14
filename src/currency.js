21f2ce02289055822ff94de14a7820c3209e2150
tyler quinn <tyler.quinn032@gmail.com>
2023-01-14 15:18:54 -0800

    added blank html, and index, and api set up

---
+++ src/currency.js
@@ -0,0 +1,16 @@
+export default class ExchangeRateService {  
+  static conversionRate() {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+      .then(function(response) {
+        if (!response.ok) {
+          const errorMessage = `${response.result} ${response['error-type']}`;
+          throw new Error(errorMessage);
+        } else {
+          return response.json();
+        }   
+      })
+      .catch(function(error) {
+        return error;
+      });
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';	
+import './css/styles.css';
+import ExchangeRateService from './currency';
+
+function conversionRate() {
+  ExchangeRateService.conversionRate()
+    .then(function(rateResponse) {
+      if (rateResponse instanceof Error) {
+        const errorMessage = `There was a problem accessing the rate data from ExchangeRate-API:
+        ${rateResponse.message}`;
+        throw new Error(errorMessage);
+      }
+    
+    })
+    .catch(function(error) {
+      document.querySelector('body').innerText = error;
+    });
+}
