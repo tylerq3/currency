693c892db32631706aa4fb3237e92abf44b4057b
tyler quinn <tyler.quinn032@gmail.com>
2023-01-14 16:18:28 -0800

    WIP stuck on display results

--- src/index.js
+++ src/index.js
@@ -3,20 +3,36 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import ExchangeRateService from './currency';
 
-function conversionRate() {
-  ExchangeRateService.conversionRate()
-    .then(function(rateResponse) {
-      if (rateResponse instanceof Error) {
+function displayExchangeRate(selectedCurrency) {
+  ExchangeRateService.conversionRate(selectedCurrency)
+    .then(function(response) {
+      if (response instanceof Error) {
         const errorMessage = `There was a problem accessing the rate data from ExchangeRate-API:
-        ${rateResponse.message}`;
+        ${response.message}`;
         throw new Error(errorMessage);
       }
-      document.querySelector('convresults').innerText = rateResponse;
+      let rate = response.conversion_rates[selectedCurrency];
+      document.querySelector('results').innerText = rate;
     })
     .catch(function(error) {
       document.querySelector('body').innerText = error;
     });
 }
 
+// UI Logic
+
+function displayConversion(response) {
+  let displayConversion = document.querySelector('results');
+  displayConversion.innerText= null;
+  
+}
 
 
+function handleForm(event) {
+  event.preventDefault(); 
+  conversionRate();
+}
+
+window.addEventListener('load', function() {
+  document.querySelector('form').addEventListener('submit', handleForm);
+});
